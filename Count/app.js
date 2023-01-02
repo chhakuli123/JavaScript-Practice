002a9c726225bef38ffcbf13f99cb7ce97520ef8
Chhakuli <chhakulizingare12@gmail.com>
2023-01-02 09:31:03 +0530

    Counter

---
+++ Count/app.js
@@ -0,0 +1,11 @@
+
+let count = 0
+
+function add() {
+    count++
+    document.getElementById("heading").innerText = count
+}
+function minus() {
+    count--
+    document.getElementById("heading").innerText = count
+}
