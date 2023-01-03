05ad042ce64ed6dad1d1d3d5ab6055c49207d42a
Chhakuli <chhakulizingare12@gmail.com>
2023-01-03 11:25:10 +0530

    string or not

---
+++ 04String or not/app.js
@@ -0,0 +1,16 @@
+
+
+let input = document.getElementById("value")
+let btn = document.getElementById("btn")
+btn.addEventListener("click", check);
+
+function check() {
+    let val = input.value
+    if (isNaN(val)) {
+        document.getElementById("heading").innerText = "Entered value is String"
+    }
+    else {
+        document.getElementById("heading").innerText = "Entered value is not a String"
+
+    }
+}
